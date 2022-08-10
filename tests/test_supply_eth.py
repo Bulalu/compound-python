1fd2d66fc0b99e705d948990aa56b315cd9a19c4
Bulalu <elishabulalu031@gmail.com>
2022-08-10 21:40:42 +0300

    structured fixtures

--- tests/test_supply_eth.py
+++ tests/test_supply_eth.py
@@ -1,23 +1,13 @@
 import pytest
 from brownie import Contract, chain, accounts
 from web3 import Web3
+
+
 one_hour = 3600
 one_day = one_hour * 24
 one_week = one_day * 7
 one_month = one_day * 30
 
-@pytest.fixture
-def cETH():
-    yield Contract.from_explorer("0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5")
-
-# @pytest.fixture
-# def pool():
-#     yield Contract.from_explorer("0x1338b4065e25AD681c511644Aa319181FC3d64CC")
-
-
-@pytest.fixture
-def whale(accounts):
-    yield accounts.at("0x6E685A45Db4d97BA160FA067cB81b40Dfed47245", force=True)
 
 def test_supply_eth(cETH, whale):
 
